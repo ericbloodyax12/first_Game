fd25f814f57bf6d8c03c57bd9568129e7669a7c0
Young Eric <erikbonopart12@gmail.com>
2022-10-27 16:51:03 +0300

    add comments

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 function getRandomHealht(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-let myUser= {
+let myUser= {                                    //создаем объект с именем силой и здоровьем перса
   name: "",
   strength:undefined,
   health:undefined,
 };
 
-myUser.health= getRandomHealht(50,100);
+myUser.health= getRandomHealht(50,100);         //задаем герою рандом знач здор
 myUser.name=prompt("Ваше имя")
 
 function createMyStrength() {
 
   let localstrength=+prompt("add strength")
-  if (isNaN(localstrength) || localstrength<=0 || localstrength>20){
+  if (isNaN(localstrength) || localstrength<=0 || localstrength>20){ //условие на силу (условие задачи)
     alert("Введите от 0 до 20")
     createMyStrength()
   }
