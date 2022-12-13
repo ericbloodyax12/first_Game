6d145fb9666fb085388a5505068e68abf29da5e6
Young Eric <erikbonopart12@gmail.com>
2022-12-13 16:22:57 +0300

    Add comments of localStorage

--- helpers/localStorageHelper.js
+++ helpers/localStorageHelper.js
@@ -1,5 +1,9 @@
-//   localStorage.setItem("lastname", "Smith"); установить данные в localStorage по ключу lastName
-//   localStorage.getItem("name"); получает данные из локал стордж по ключу нэйм
+//  1. localStorage.setItem("lastname", "Smith"); установить данные в localStorage по ключу lastName
+//  2. localStorage.getItem("name"); получает данные из локал стордж по ключу нэйм
+//  3. localStorage.removeItem("key"); удаляет данный из локал стродж по ключу
+//  4. localStorage.clear(); удаляет все
+//  5. метод работает только со строками, с помощью JSON.stringify(object) можно хранить к примеру объекты, но данные будут храниться все также в виде строки
+//  6.   чтобы взять что то сложное типо объекта из локал стордж можно также вернуться к п.2 и затем JSON.parse(name)
 
 export const setDataToLocalStorage = (value) => {
     localStorage.setItem("name", value);
